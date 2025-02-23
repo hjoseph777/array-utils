e9b029787d7a5798173618045206cd331ed1f152
H Joseph <hjoseph777@gmail.com>
2025-02-23 17:24:02 -0500

    main

--- src/main/java/com/example/arrayutils/ArrayOperationsTester.java
+++ src/main/java/com/example/arrayutils/ArrayOperationsTester.java
@@ -2,29 +2,27 @@ package com.example.arrayutils;
 
 import java.util.Scanner;
 
-// ArrayOperationsTester Class
 class ArrayOperationsTester {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+
         while (true) {
             DynamicArrayManager manager = new DynamicArrayManager();
-            manager.promptAndPopulateArray();
-            
-            // Sample Run
-            manager.insertNumberAtPosition();
-            manager.removeNumberAndShift();
-            
+            manager.initializeArray();
+
+            manager.insertNumber();
+            manager.removeNumber();
+
             System.out.println("Choose an option:");
             System.out.println("C - Continue");
             System.out.println("X - Exit");
             String choice = scanner.next().toUpperCase();
-            
+
             if (choice.equals("X")) {
                 break;
             }
         }
-        
+
         scanner.close();
     }
 }
--- src/main/java/com/example/arrayutils/DynamicArrayManager.java
+++ src/main/java/com/example/arrayutils/DynamicArrayManager.java
@@ -2,36 +2,35 @@ package com.example.arrayutils;
 
 import java.util.Scanner;
 
-// DynamicArrayManager Class
 class DynamicArrayManager {
     private int[] array;
-    private Scanner scanner = new Scanner(System.in);
+    private final Scanner scanner = new Scanner(System.in);
 
-    void promptAndPopulateArray() {
+    void initializeArray() {
         System.out.print("Enter the initial size of the array: ");
         int size = scanner.nextInt();
         array = new int[size];
-        for (int i = 1; i <= size; i++) {
-            System.out.print("Enter number " + i + ": ");
-            array[i - 1] = scanner.nextInt();
+        for (int i = 0; i < size; i++) {
+            System.out.print("Enter number " + (i + 1) + ": ");
+            array[i] = scanner.nextInt();
         }
         System.out.println("Initial array:");
         displayArrayContents();
     }
 
-    void insertNumberAtPosition() {
+    void insertNumber() {
         System.out.print("Give the index and value to insert at that index: ");
         int index = scanner.nextInt();
         int number = scanner.nextInt();
-        array = ArrayOperations.insertNumberAtPosition(array, index, number);
+        array = ArrayOperations.insertAt(array, index, number);
         System.out.println("After inserting at index " + index + ":");
         displayArrayContents();
     }
 
-    void removeNumberAndShift() {
+    void removeNumber() {
         System.out.print("Enter the number to delete: ");
         int number = scanner.nextInt();
-        array = ArrayOperations.removeNumberAndShift(array, number);
+        array = ArrayOperations.removeAndShift(array, number);
         System.out.println("After deleting " + number + ":");
         displayArrayContents();
     }
